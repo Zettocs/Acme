8de91a190b3a05ccf554492f2022be6ce62078b5
Zettocs <valentin.scottez@outlook.fr>
2023-01-04 11:46:22 +0100

    Ajout du formulaire d'inscription connecté à la bdd

--- src/Controller/AcmeController.php
+++ src/Controller/AcmeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Inscription;
+use App\Form\InscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 class AcmeController extends AbstractController
 {
@@ -12,5 +17,24 @@ class AcmeController extends AbstractController
     public function homepage(): Response
     {
         return $this->render('acme/homepage.html.twig');
+    }
+
+    #[Route('/inscription', name:'app_inscription')]
+    public function nouvelleInscription(Request $request, PersistenceManagerRegistry $doctrine)
+    {
+        $inscription = new Inscription();
+        $form = $this->createForm(InscriptionType::class, $inscription);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            //$inscription->setCreatedAt(new \DateTime());
+            $em->persist($inscription);
+            $em->flush();
+        }
+
+        return $this->render('acme/inscription.html.twig', [
+            'formNouvelleInscription' => $form->createView()
+        ]);
     }
 }
---
+++ src/Entity/Inscription.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InscriptionRepository::class)]
+class Inscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+}
